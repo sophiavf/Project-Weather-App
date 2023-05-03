50109e858d7d3e290a52101022ece73642b49e04
sophia <15851587+sophiavf@users.noreply.github.com>
2023-05-03 15:24:57 +0200

    - added public folder - trying different vite configs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 import { createHtmlPlugin } from "vite-plugin-html";
 
@@ -11,22 +12,19 @@ export default defineConfig({
 	},
 
 	plugins: [
-		createHtmlPlugin({
-			minify: true,
-			entry: "./src/index.js",
-			template: "./src/index.html",
-			inject: {
-				data: {
-					title: "My Weather App",
-					injectScript: `<script src="./src/index.js"></script>`,
-				},
-			},
-		}),
+		// createHtmlPlugin({
+		// 	minify: true,
+		// 	entry: "./index.html",
+		// 	inject: {
+		// 		data: {
+		// 			title: "My Weather App",
+		// 			injectScript: `<script src="./src/index.js"></script>`,
+		// 		},
+		// 	},
+		// }),
 		react({
 			include: /\.(mdx|js|jsx|ts|tsx)$/,
-			babel: {
-				plugins: ["babel-plugin-styled-components"],
-			},
 		}),
+		viteTsconfigPaths(),
 	],
 });
