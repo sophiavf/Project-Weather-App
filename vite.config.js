cc7e4afb6682e6be77e7bd5493b6bd616d13e729
sophia <15851587+sophiavf@users.noreply.github.com>
2023-05-07 22:39:24 +0200

    - Added the config for deploying to GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-})
+	base: "/Project-Weather-App",
+	plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+});
